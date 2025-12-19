f8bda665d7e5c12666eae19ab9fc3ae8ce189c31
Putranugroho14 <putranugroho61@gmail.com>
2025-12-19 19:17:02 +0700

    destinasihome

---
+++ app/src/main/java/com/example/prak10/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.prak10.uicontroller.route
+
+import com.example.prak10.R
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.nama
+}
