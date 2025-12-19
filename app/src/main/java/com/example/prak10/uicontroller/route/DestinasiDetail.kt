52f94ea7572e75f8b9ccb19500168de2cc0d452b
Putranugroho14 <putranugroho61@gmail.com>
2025-12-19 19:12:43 +0700

    destinasidetail

---
+++ app/src/main/java/com/example/prak10/uicontroller/route/DestinasiDetail.kt
@@ -0,0 +1,8 @@
+package com.example.prak10.uicontroller.route
+
+import com.example.prak10.R
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "item_detail"
+    override val titleRes = R.string.detail_siswa
+}
