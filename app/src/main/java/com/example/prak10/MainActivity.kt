8c27a7530ef7e69c6e83c5f818fd524dc1f545be
Putranugroho14 <putranugroho61@gmail.com>
2025-12-19 19:19:55 +0700

    main

--- app/src/main/java/com/example/prak10/MainActivity.kt
+++ app/src/main/java/com/example/prak10/MainActivity.kt
@@ -11,17 +11,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.prak10.ui.theme.Prak10Theme
+import com.example.prak10.ui.theme.ApiTheme
+import com.example.prak10.uicontroller.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Prak10Theme {
+            ApiTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    Prak10Theme {
+    ApiTheme {
         Greeting("Android")
     }
 }
