94c0930dd83ee728ded5b80eb32dec2132a27aec
Putranugroho14 <putranugroho61@gmail.com>
2025-12-19 19:18:19 +0700

    home vm

---
+++ app/src/main/java/com/example/prak10/viewmodel/HomeViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.prak10.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.prak10.repositori.RepositoryDataSiswa
+import com.example.prak10.modeldata.DataSiswa
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
+sealed interface StatusUiSiswa {
+    data class Success(val siswa: List<DataSiswa> = listOf()) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
+
+class HomeViewModel(private val repositoryDataSiswa: RepositoryDataSiswa) :
+    ViewModel() {
+
+    var listSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
+        private set
+
+    init {
+        loadSiswa()
+    }
+
+    fun loadSiswa() {
+        viewModelScope.launch {
+            listSiswa = StatusUiSiswa.Loading
+            try {
+                listSiswa = StatusUiSiswa.Success(repositoryDataSiswa.getDataSiswa())
+            } catch (e: IOException) {
+                StatusUiSiswa.Error
+            } catch (e: HttpException) {
+                StatusUiSiswa.Error
+            }
+        }
+    }
+}
