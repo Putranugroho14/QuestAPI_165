0001d4cdd4f97da3840cc6182c9eaae1cee0fe60
Putranugroho14 <putranugroho61@gmail.com>
2025-12-19 10:37:43 +0700

    buat data class data siswa val id : int

---
+++ app/src/main/java/com/example/prak10/modeldata/DataSiswa.kt
@@ -0,0 +1,14 @@
+package com.example.prak10.modeldata
+
+import com.example.prak10.modeldata.DetailSiswa
+import kotlinx.serialization.Serializable
+import kotlin.Int
+
+@Serializable
+data class DataSiswa (
+    val id : Int,
+
+)
+
+
+
