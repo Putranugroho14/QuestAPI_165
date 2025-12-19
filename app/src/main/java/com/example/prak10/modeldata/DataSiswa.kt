640407e3a13427089b66a02a176001dd7647468c
Putranugroho14 <putranugroho61@gmail.com>
2025-12-19 10:46:39 +0700

    fun DataSiswa.toUiStateSiswa   isEntryValid = isEntryValid

--- app/src/main/java/com/example/prak10/modeldata/DataSiswa.kt
+++ app/src/main/java/com/example/prak10/modeldata/DataSiswa.kt
@@ -35,6 +35,6 @@ fun DetailSiswa.toDataSiswa(): DataSiswa = DataSiswa(
 fun DataSiswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa =
     UIStateSiswa(
         detailSiswa = this.toDetailSiswa(),
-
+        isEntryValid = isEntryValid
     )
 
