0d12867e9feb8a276b7aa0fd36f7797f6b075eca
Putranugroho14 <putranugroho61@gmail.com>
2025-12-26 20:00:35 +0700

    override val route = "item_edit"

--- app/src/main/java/com/example/prak10/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/prak10/uicontroller/route/DestinasiDetail.kt
@@ -3,6 +3,8 @@ package com.example.prak10.uicontroller.route
 import com.example.prak10.R
 
 object DestinasiDetail : DestinasiNavigasi {
-    override val route = "item_detail"
+    override val route = "detail_siswa"
     override val titleRes = R.string.detail_siswa
+    const val ITEM_ID_ARG= "idSiswa"
+    val routeWithArgs = "$route/{$ITEM_ID_ARG}"
 }
