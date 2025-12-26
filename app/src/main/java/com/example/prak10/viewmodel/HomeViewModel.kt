9c6b7cfe61f0bded96ef4d1aaab35723e1539954
Putranugroho14 <putranugroho61@gmail.com>
2025-12-26 19:59:19 +0700

    home view model

--- app/src/main/java/com/example/prak10/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/prak10/viewmodel/HomeViewModel.kt
@@ -5,8 +5,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.prak10.repositori.RepositoryDataSiswa
 import com.example.prak10.modeldata.DataSiswa
+import com.example.prak10.repositori.RepositoryDataSiswa
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
 import java.io.IOException
@@ -30,8 +30,8 @@ class HomeViewModel(private val repositoryDataSiswa: RepositoryDataSiswa) :
     fun loadSiswa() {
         viewModelScope.launch {
             listSiswa = StatusUiSiswa.Loading
-            try {
-                listSiswa = StatusUiSiswa.Success(repositoryDataSiswa.getDataSiswa())
+            listSiswa = try {
+                StatusUiSiswa.Success(repositoryDataSiswa.getDataSiswa())
             } catch (e: IOException) {
                 StatusUiSiswa.Error
             } catch (e: HttpException) {
