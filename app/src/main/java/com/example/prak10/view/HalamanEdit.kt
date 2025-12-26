0e56d2c207debbc46fbc1ccb0811d1ba3c144235
Putranugroho14 <putranugroho61@gmail.com>
2025-12-26 19:58:57 +0700

    halaman edit

--- app/src/main/java/com/example/prak10/view/HalamanEdit.kt
+++ app/src/main/java/com/example/prak10/view/HalamanEdit.kt
@@ -1,4 +1,47 @@
 package com.example.prak10.view
 
-class HalamanEdit {
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.prak10.viewmodel.provider.PenyediaViewModel
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.res.stringResource
+import androidx.compose.runtime.rememberCoroutineScope
+import kotlinx.coroutines.launch
+import androidx.compose.foundation.layout.padding
+import com.example.prak10.viewmodel.EditViewModel
+import com.example.prak10.uicontroller.route.DestinasiEdit
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditSiswaScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiEdit.titleRes),
+                canNavigateBack = true,
+                navigateUp = onNavigateUp
+            )
+        },
+        modifier = modifier
+    ){ innerPadding ->
+        val coroutineScope = rememberCoroutineScope()
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUIState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.editSatuSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
 }
