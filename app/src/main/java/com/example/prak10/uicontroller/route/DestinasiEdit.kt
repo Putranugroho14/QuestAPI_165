509d429cbc3129de48e54ed4b92a55aa344e7d48
Putranugroho14 <putranugroho61@gmail.com>
2025-12-26 20:01:02 +0700

    override val titleRes = R.string.edit_siswa

---
+++ app/src/main/java/com/example/prak10/uicontroller/route/DestinasiEdit.kt
@@ -0,0 +1,10 @@
+package com.example.prak10.uicontroller.route
+
+import com.example.prak10.R
+
+object DestinasiEdit: DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+
+
+}
